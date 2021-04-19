39d54c3144576d02402b94aa0ba12a2c869e9d2f
Francisco Oliveira <55900260+washolv@users.noreply.github.com>
2021-04-19 15:04:17 -0300

    ajustes

--- src/app/modules/checkout/checkout.module.ts
+++ src/app/modules/checkout/checkout.module.ts
@@ -2,11 +2,9 @@ import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { RouterModule, Routes } from '@angular/router';
-import { EnderecoEntregaComponent } from './endereco-entrega/endereco-entrega.component';
 
 const routes: Routes = [
   { path: '', component: CheckoutComponent },
-  { path: 'endereco-entrega', component: EnderecoEntregaComponent}
 ];
 
 @NgModule({
@@ -14,7 +12,7 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
   ],
-  declarations: [CheckoutComponent, EnderecoEntregaComponent],
+  declarations: [CheckoutComponent],
   exports: [
     RouterModule,
   ],
--- src/app/modules/produto/produto.module.ts
+++ src/app/modules/produto/produto.module.ts
@@ -1,22 +1,13 @@
-import { AdicionarEstoqueComponent } from './modals/adicionar-estoque/adicionar-estoque.component';
 import { CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule, CurrencyPipe, registerLocaleData } from '@angular/common';
-import { ProdutoComponent } from './produto/produto.component';
-import { ModalExcluirProdutoComponent } from './modals/modal-excluir-produto/modal-excluir-produto.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
-import { EditarProdutoComponent } from './editar-produto/editar-produto.component';
-import { AdicionarProdutoComponent } from './adicionar-produto/adicionar-produto.component';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { VisualizarProdutoComponent } from './visualizar-produto/visualizar-produto.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { AdicionarImagensProdutoComponent } from './adicionar-produto/adicionar-imagens-produto/adicionar-imagens-produto.component';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
-import { EditarImagensProdutoComponent } from './editar-produto/editar-imagens-produto/editar-imagens-produto.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { NgbModule, NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
@@ -25,12 +16,6 @@ import localePt from '@angular/common/locales/pt';
 
 
 const routes: Routes = [
-  { path: '', component: ProdutoComponent },
-  { path: 'editar/:id', component: EditarProdutoComponent,/*canActivate: [AdminGuard]*/ },
-  { path: 'adicionar', component: AdicionarProdutoComponent, /*canActivate: [AdminGuard],*/},
-  { path: 'visualizar/:id', component: VisualizarProdutoComponent},
-  { path: 'adicionar/imagens', component: AdicionarImagensProdutoComponent, /*canActivate: [AdminGuard]*/},
-  { path: 'editar/imagens/:id', component: EditarImagensProdutoComponent,/* canActivate: [AdminGuard]*/}
 ];
 registerLocaleData(localePt, 'pt');
 
@@ -54,14 +39,7 @@ registerLocaleData(localePt, 'pt');
     MatIconModule,
   ],
   declarations: [
-    ProdutoComponent,
-    ModalExcluirProdutoComponent,
-    EditarProdutoComponent,
-    AdicionarProdutoComponent,
-    VisualizarProdutoComponent,
-    AdicionarImagensProdutoComponent,
-    EditarImagensProdutoComponent,
-    AdicionarEstoqueComponent
+
   ],
   exports: [
     RouterModule,
@@ -74,7 +52,6 @@ registerLocaleData(localePt, 'pt');
     }
   ],
   entryComponents: [
-    ModalExcluirProdutoComponent,
   ],
   bootstrap: [
   ], schemas: [CUSTOM_ELEMENTS_SCHEMA]
